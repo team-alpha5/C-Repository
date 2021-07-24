9f5d2e8120c776f317e1533d953648f81fadd4ad
kail139 <39454338+kail139@users.noreply.github.com>
2021-07-24 09:45:27 +0530

    Add files via upload

---
+++ Hexa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+
+  int a;
+  printf("\n***This Program Prints The Given Integer Value In Octal And Hexadecimal!***");
+  printf("\n\n\nEnter A Integer Number:");
+  scanf("%d",&a);
+  printf("\n\nThe Octal Value Of %d Is %o !",a,a);
+  printf("\n\nThe Hexadecimal Value Of %d Is %x",a,a);
+
+return 0;
+}
